d2f23eb37b1019dbd58e6efd1780d153b0e82ff0
cedric-winbush <cawinbushjr@gmail.com>
2020-12-15 15:05:01 -0600

    adds hooks folder

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,18 +1,18 @@
 import React, { useState } from 'react'
 
 const UploadForm = () => {
+  const [file, setFile ] = useState(null)
+  const [error, setError ] = useState(null);
+  
+  const types = ['image/png', 'image/jpeg'];
   
   const handleChange = (e) => {
-    const [file, setFile ] = useState(null)
-    const [error, setError ] = useState(null;
-
-    const types = ['image/png', 'image/jpeg'];
-    // e.preventDefault()
     let selected = e.target.files[0];
     console.log(selected)
 
-    if(selected &&  types.includes(selected.type)) {
+    if(selected && types.includes(selected.type)) {
       setFile(selected);
+      setError('')
     } else {
       setFile(null)
       setError('Please select an image file (png or jpeg)');
@@ -22,6 +22,11 @@ const UploadForm = () => {
   return (
     <form>
       <input type="file" onChange={handleChange}/>
+      <div className="output">
+
+        { error && <div className="error"> {error} </div>  }
+        {file && <div>{file.name}</div>}
+      </div>
     </form>
   )
 }
