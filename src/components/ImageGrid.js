07e85de55662ea91f00c56929f37762c9a2b6d45
cedric-winbush <cawinbushjr@gmail.com>
2020-12-16 17:06:48 -0600

    adds imagegrid component to display image

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import useFirestore from '../hooks/useFirestore'
 
 const ImageGrid = () => {
+  const { docs } = useFirestore('images')
+  console.log(docs);
   return (
     <div className="img-grid">
-      images
+      {docs && docs.map(doc => (
+        <div className="img-wrap" key={doc.id}>
+          <img src={doc.url} alt='image' />
+        </div>
+      ))}
     </div>
   )
 }
