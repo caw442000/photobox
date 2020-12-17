87ec1905363613fb4a4fe93a9c85e8d716ff8e25
cedric-winbush <cawinbushjr@gmail.com>
2020-12-16 18:31:15 -0600

    adds delete functionality

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -1,18 +1,31 @@
-import React from 'react'
-import useFirestore from '../hooks/useFirestore'
+import React from "react";
+import { projectFirestore } from "../firebase/config";
+import useFirestore from "../hooks/useFirestore";
 
 const ImageGrid = () => {
-  const { docs } = useFirestore('images')
+  const { docs } = useFirestore("images");
   console.log(docs);
+  const handleClick = (e, id) => {
+    e.preventDefault()
+    console.log(id)
+    projectFirestore.collection("images").doc(id).delete().then(()=> {
+      console.log("deleted")
+    })
+
+  }
   return (
     <div className="img-grid">
-      {docs && docs.map(doc => (
-        <div className="img-wrap" key={doc.id}>
-          <img src={doc.url} alt='image' />
-        </div>
-      ))}
+      {docs &&
+        docs.map((doc) => (
+          <div key={doc.id}>
+            <div className="img-wrap" >
+              <img src={doc.url} alt="image" />
+            </div>
+            <button style={{ zIndex: 100, color:" var(--primary)" }} onClick={(e) => handleClick(e, doc.id)}>Delete</button>
+          </div>
+        ))}
     </div>
-  )
-}
+  );
+};
 
-export default ImageGrid
+export default ImageGrid;
